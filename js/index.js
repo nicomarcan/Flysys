b3c764a753a3e05a7e1896b262b4c6b59379b24f
nicomarcan <marcantonionicolas@gmail.com>
2016-10-06 12:31:48 -0300

    js update

--- js/index.js
+++ js/index.js
@@ -4,11 +4,17 @@ $(document).ready(function(){
 			 if(!$(this).attr("selected")){
 			 	$('li.clickable').removeAttr("selected");
 			 	$(this).attr("selected","");
+			 	var from = $('#from');
+				var to = $('#to')
 			 	if($(this).attr("id")=="one-way"){
 					$('#returning').hide();
+					from.attr("class","input-field col s3");
+					to.attr("class","input-field col s3 ");
 				 }
 				 else if($(this).attr("id")=="two-way"){
 				 	$('#returning').show();
+				 	from.attr("class","input-field col s2 ");
+					to.attr("class","input-field col s2 ");
 				 }
 			  }
 		});
