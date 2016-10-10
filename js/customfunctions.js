7e0486ed06b0d5680249a05c494149836ee96a1e
mota <mepuedeayudar@hotmail.com>
2016-10-10 15:41:48 -0300

    fixed ajax function

--- js/customfunctions.js
+++ js/customfunctions.js
@@ -1,4 +1,4 @@
-function fajax(aurl,fsuccess){
+function fajax(aurl,fsuccess,ferror){
     var urlfinal="http://hci.it.itba.edu.ar/v1/api/misc.groovy?"+aurl;
     console.log('Iniciando ajax a:'+urlfinal);
     $.ajax({
@@ -6,10 +6,10 @@ function fajax(aurl,fsuccess){
         dataType: 'json',
         url: urlfinal,
         success: function(data){
-            if(!data.error==undefined){
-                console.log("ERROR REQUEST"+data.error.message+" in request:"+'url');
-            }else{
+            if(data.error==undefined){
                 fsuccess(data);
+            }else{
+                console.log("ERROR REQUEST:"+data.error.message+" in request "+urlfinal);
             }
         },
         error: function(jqxhr,status,errorthrown) {
