0c6590b08a02f1265c719e8dd15f43288cd51b92
mota <mepuedeayudar@hotmail.com>
2016-10-16 02:54:17 -0300

    hecho detalle

--- js/common.js
+++ js/common.js
@@ -10,25 +10,13 @@ function fajax(aurl,datos,fsuccess,ferror){
             if(data.error==undefined){
                 fsuccess(data);
             }else{
-                console.log("ERROR REQUEST:"+data.error.message+" in request "+urlfinal);
+                console.log("ERROR REQUEST: "+data.error.message+"     in request "+urlfinal);
             }
         },
         error: function(jqxhr,status,errorthrown) {
             ferror();
         }
     });
-}
-//DEPRECATED: getUrlParameter hace lo mismo
-function getGETparam(paramName) {
-    var query = location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (decodeURIComponent(pair[0]) === paramName) {
-            return decodeURIComponent(pair[1]);
-        }
-    }
-    return null;
 }
 
 function getUrlParameter(sParam) {
--- js/detalle.js
+++ js/detalle.js
@@ -19,14 +19,35 @@ function addCard() {
     phone: contacto.phones[0],
     email: contacto.email
     });
-  $('#facturacion').append(rendered);
+  $('#confirmar').before(rendered);
+}
+var vueloejemplo={"price":{"adults":{"base_fare":278.00,"quantity":1},"children":{"base_fare":66.00,"quantity":1},"infants":{"base_fare":0.00,"quantity":1},"total":{"charges":13.76,"taxes":68.80,"fare":344.00,"total":426.56}},"outbound_routes":[{"segments":[{"arrival":{"date":"2016-12-25 07:09:00","airport":{"id":"TUC","description":"Aeropuerto Benjamin Matienzo, San Miguel de Tucuman, Argentina","time_zone":"-03:00","city":{"id":"TUC","name":"San Miguel de Tucuman, Tucuman","country":{"id":"AR","name":"Argentina"}}}},"departure":{"date":"2016-12-25 05:50:00","airport":{"id":"EZE","description":"Aeropuerto Ezeiza Ministro Pistarini, Buenos Aires, Argentina","time_zone":"-03:00","city":{"id":"BUE","name":"Buenos Aires, Ciudad de Buenos Aires","country":{"id":"AR","name":"Argentina"}}}},"id":94588,"number":8700,"cabin_type":"ECONOMY","airline":{"id":"8R","name":"SOL","rating":9.00},"duration":"01:19","stopovers":[]}],"duration":"01:19"}]};
+
+function addFlight(){
+  var template = $('#detallevuelo').html();
+  Mustache.parse(template);
+  var vuelos=getLocalObject("flights");
+  for (var i = 0; i < vuelos.length; i++) {
+    var vuelo=vuelos[i];
+    var rendered = Mustache.render(template,{
+      id_vuelo: vuelo.outbound_routes[0].segments[0].id,
+      fecha_vuelo: vuelo.outbound_routes[0].segments[0].arrival.date,
+      hora_sal_vuelo: vuelo.outbound_routes[0].segments[0].arrival.date,
+      hora_lle_vuelo: vuelo.outbound_routes[0].segments[0].arrival.date,
+      origen_vuelo: vuelo.outbound_routes[0].segments[0].arrival.airport.city.name,
+      destino_vuelo: vuelo.outbound_routes[0].segments[0].departure.airport.city.name,
+      precio_adulto_vuelo: vuelo.price.adults.base_fare,
+      precio_chico_vuelo:  vuelo.price.children.base_fare,
+      precio_infante_vuelo: vuelo.price.infants.base_fare
+      });
+    $('#detalle').append(rendered);
+  }
 }
 function addPassagers() {
   var template = $('#detallepasajero').html();
   Mustache.parse(template);
   var pasajeros=getLocalObject("passengers");
   for (var i = 0; i < pasajeros.length; i++) {
-    console.log(pasajeros[i]);
     var pas=pasajeros[i];
     var rendered = Mustache.render(template,{
       idpasajero:i,
@@ -36,12 +57,23 @@ function addPassagers() {
       passport: pas.id_number
 
       });
-    $('#correcto').before(rendered);
+    $('#primero').after(rendered);
   }
 
 }
 
 $(document).ready(function(){
+  setLocalObject("flights",[vueloejemplo]);
+  addFlight();
   addCard();
   addPassagers();
+  var final={
+    flight_id: "93480",
+    passengers: getLocalObject("passengers"),
+    payment: getLocalObject("payment"),
+    contact: getLocalObject("contact")
+  }
+  $("#confirmar").click(function(){
+      fajax("http://hci.it.itba.edu.ar/v1/api/booking.groovy?method=bookflight",final,console.log,undefined);
+  });
 });
